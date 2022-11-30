9bebab897ec9a7fc334014e6c3d6aaa69bd8b3ec
DANIELA MUNOZ <danielamun.832@gmail.com>
2022-11-30 12:03:23 -0600

    js-connection

--- js/site.js
+++ js/site.js
@@ -1,1 +1,15 @@
 //Team: This java file will be used for all pages
+
+//Contact page
+function checkForm() {
+    var firstName = document.forms["contactForm"]["first_name"].value
+    var lastName = document.forms["contactForm"]["last_name"].value
+    var email= document.forms["contactForm"]["email_info"].value
+    if ((!firstName) || (!lastName) || (!email)) {
+        alert("Please all the necessary fields");
+        return false;
+    }
+    else {
+        alert("You have successfully submitted this form. Thank you!");
+    }
+}
